7d68653603437f3c687896c8b9efc435d1d39ee6
sanzaru <sanzaru@users.noreply.github.com>
2022-07-03 13:29:40 +0200

    Drag gesture now based on edge the toast is attached to Minor code clean up

--- Sources/SimpleToast/SimpleToast.swift
+++ Sources/SimpleToast/SimpleToast.swift
@@ -16,25 +16,100 @@ struct SimpleToast<SimpleToastContent: View>: ViewModifier {
 
     @State private var timer: Timer?
     @State private var offset: CGSize = .zero
+    @State private var delta: CGFloat = 0
 
-    private let toastContent: SimpleToastContent
+    private let toastInnerContent: SimpleToastContent
+
+    private let maxDelta: CGFloat = 20
 
     /// Dimiss the toast on drag
-    /// TODO: Needs better implementation.
     private var dragGesture: some Gesture {
         DragGesture()
             .onChanged { [self] in
-                if $0.translation.height < offset.height {
-                    offset.height = $0.translation.height
+                delta = 0
+
+                switch options.alignment {
+                case .top, .topLeading, .topTrailing:
+                    if $0.translation.height <= offset.height {
+                        offset.height = $0.translation.height
+                    }
+                    delta += abs(offset.height)
+
+                case .bottom, .bottomLeading, .bottomTrailing:
+                    if $0.translation.height >= offset.height {
+                        offset.height = $0.translation.height
+                    }
+                    delta += abs(offset.height)
+
+                case .leading:
+                    if $0.translation.width <= offset.width {
+                        offset.width = $0.translation.width
+                    }
+                    delta += abs(offset.width)
+
+                case .trailing:
+                    if $0.translation.width >= offset.width {
+                        offset.width = $0.translation.width
+                    }
+                    delta += abs(offset.width)
+
+                default:
+                    if $0.translation.height < offset.height {
+                        offset.height = $0.translation.height
+                    }
+                    delta += abs(offset.height)
                 }
             }
             .onEnded { [self] _ in
-                if offset.height <= -20 {
-                    dismiss()
+                if delta >= maxDelta {
+                    return dismiss()
                 }
 
                 offset = .zero
             }
+    }
+
+    @ViewBuilder
+    private var toastRenderContent: some View {
+        if showToast {
+            Group {
+                switch options.modifierType {
+                case .slide:
+                    toastInnerContent
+                        .modifier(SimpleToastSlide(showToast: $showToast, options: options))
+                        .gesture(dragGesture)
+                        .onTapGesture(perform: dismiss)
+                        .offset(offset)
+
+                case .scale:
+                    toastInnerContent
+                        .modifier(SimpleToastScale(showToast: $showToast, options: options))
+                        .gesture(dragGesture)
+                        .onTapGesture(perform: dismiss)
+                        .offset(offset)
+
+                case .skew:
+                    toastInnerContent
+                        .modifier(SimpleToastSkew(showToast: $showToast, options: options))
+                        // .gesture(dragGesture)
+                        .onTapGesture(perform: dismiss)
+                        .offset(offset)
+
+                case .curtain:
+                    toastInnerContent
+                        .modifier(SimpleToastCurtain(showToast: $showToast, options: options))
+                        .onTapGesture(perform: dismiss)
+
+                default:
+                    toastInnerContent
+                        .modifier(SimpleToastFade(showToast: $showToast, options: options))
+                        .gesture(dragGesture)
+                        .onTapGesture(perform: dismiss)
+                        .offset(offset)
+                }
+            }
+            .onAppear(perform: dismissAfterTimeout)
+        }
     }
 
     init(
@@ -46,7 +121,7 @@ struct SimpleToast<SimpleToastContent: View>: ViewModifier {
         self._showToast = showToast
         self.options = options
         self.onDismiss = onDismiss
-        self.toastContent = content()
+        self.toastInnerContent = content()
     }
 
     func body(content: Content) -> some View {
@@ -64,43 +139,7 @@ struct SimpleToast<SimpleToastContent: View>: ViewModifier {
             )
 
             // Toast content
-            .overlay(
-                VStack {
-                    if showToast {
-                        Group {
-                            switch options.modifierType {
-                            case .slide:
-                                toastContent
-                                    .modifier(SimpleToastSlide(showToast: $showToast, options: options))
-                                    .gesture(dragGesture)
-                                    .onTapGesture(perform: dismiss)
-                                    .offset(offset)
-
-                            case .scale:
-                                toastContent
-                                    .modifier(SimpleToastScale(showToast: $showToast, options: options))
-                                    .gesture(dragGesture)
-                                    .onTapGesture(perform: dismiss)
-                                    .offset(offset)
-
-                            case .skew:
-                                toastContent
-                                    .modifier(SimpleToastSkew(showToast: $showToast, options: options))
-                                    // .gesture(dragGesture)
-                                    .onTapGesture(perform: dismiss)
-                                    .offset(offset)
-
-                            default:
-                                toastContent
-                                    .modifier(SimpleToastFade(showToast: $showToast, options: options))
-                                    .gesture(dragGesture)
-                                    .onTapGesture(perform: dismiss)
-                            }
-                        }
-                        .onAppear(perform: dismissAfterTimeout)
-                    }
-                }, alignment: options.alignment
-            )
+            .overlay(toastRenderContent, alignment: options.alignment)
     }
 
     /// Dismiss the sheet after the timeout specified in the options
@@ -127,27 +166,7 @@ struct SimpleToast<SimpleToastContent: View>: ViewModifier {
 }
 
 // MARK: - View extensions
-
 public extension View {
-    /// Present the sheet based on the state of a given binding to a boolean.
-    ///
-    /// - NOTE: The toast will be attached to the view's frame it is attached to and not the general UIScreen.
-    /// - Parameters:
-    ///   - isShowing: Boolean binding as source of truth for presenting the toast
-    ///   - options: Options for the toast
-    ///   - onDismiss: Closure called when the toast is dismissed
-    ///   - content: Inner content for the toast
-    /// - Returns: The toast view
-    @available(*, deprecated, renamed: "simpleToast(isPresented:options:onDismiss:content:)")
-    func simpleToast<SimpleToastContent: View>(
-        isShowing: Binding<Bool>, options: SimpleToastOptions,
-        onDismiss: (() -> Void)? = nil,
-        @ViewBuilder content: @escaping () -> SimpleToastContent) -> some View {
-        self.modifier(
-            SimpleToast(showToast: isShowing, options: options, onDismiss: onDismiss, content: content)
-        )
-    }
-
     /// Present the sheet based on the state of a given binding to a boolean.
     ///
     /// - NOTE: The toast will be attached to the view's frame it is attached to and not the general UIScreen.
@@ -192,6 +211,28 @@ public extension View {
 
         return self.modifier(
             SimpleToast(showToast: bindingProxy, options: options, onDismiss: onDismiss, content: content)
+        )
+    }
+}
+
+// MARK: - Deprecated
+public extension View {
+    /// Present the sheet based on the state of a given binding to a boolean.
+    ///
+    /// - NOTE: The toast will be attached to the view's frame it is attached to and not the general UIScreen.
+    /// - Parameters:
+    ///   - isShowing: Boolean binding as source of truth for presenting the toast
+    ///   - options: Options for the toast
+    ///   - onDismiss: Closure called when the toast is dismissed
+    ///   - content: Inner content for the toast
+    /// - Returns: The toast view
+    @available(*, deprecated, renamed: "simpleToast(isPresented:options:onDismiss:content:)")
+    func simpleToast<SimpleToastContent: View>(
+        isShowing: Binding<Bool>, options: SimpleToastOptions,
+        onDismiss: (() -> Void)? = nil,
+        @ViewBuilder content: @escaping () -> SimpleToastContent) -> some View {
+        self.modifier(
+            SimpleToast(showToast: isShowing, options: options, onDismiss: onDismiss, content: content)
         )
     }
 }
